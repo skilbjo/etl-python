627f8c51c1d7a9c86f8c6e7fc9c586731947ef27
John Skilbeck <john.skilbeck@gmail.com>
2016-05-13 16:56:17 -0700

    cron

--- sql/MPR/load_mpr.sql
+++ sql/MPR/load_mpr.sql
@@ -3,5 +3,5 @@ insert into mpr(
 	ParentAccountId, ParentName, FeePaymentType, PaymentTypeGroup, 
 	TPV_USD , Refunds_Chargebacks, TPV_Net_USD, Txn_Count, Revenue, COGS_USD
 	) values (
-		%s,%s,%s,%s,%s%s,%s,%s,%s,%s%s,%s,%s,%s,%s
+		%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
 	)
