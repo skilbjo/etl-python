6a6bd2c2ed1a52a15d77ff6c71b4dd26335ca28e
John Skilbeck <john.skilbeck@gmail.com>
2016-05-23 16:00:12 -0700

    aggregate table

--- sql/MPR/load_aggregate_volume.sql
+++ sql/MPR/load_aggregate_volume.sql
@@ -1,7 +1,7 @@
 insert into aggregate_volume (
-	Date, PlatformId, ParentAccountId, ParentName,
+	Date, PlatformId, ParentAccountId, ParentName, DateFirstSeen,
 	Gateway, FeePaymentType, IssuerType, PaymentTypeGroup,  
 	TPV_USD, TPV_Net_USD
 	) values (
-		%s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+		%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
 )
